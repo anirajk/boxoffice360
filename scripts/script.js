ea5f8885c074a75092553163aca3621db890c602
ashwinijanamatti <ashwini.janamatti@utah.edu>
2016-11-08 11:57:21 -0700

    8th November 11:58

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,20 @@
  * Created by Ashwini on 11/7/2016.
  */
 
+d3.select('#tab1')
+.on('click',function(){
+
+    d3.select('#content2').attr('class','hide');
+    d3.select('#content1').attr('class','show');
+    d3.select('#tab1').style('background-color','steelblue');
+    d3.select('#tab2').style('background-color','buttonface');
+});
+
+d3.select('#tab2')
+    .on('click',function(){
+
+        d3.select('#content1').attr('class','hide');
+        d3.select('#content2').attr('class','show');
+        d3.select('#tab2').style('background-color','steelblue');
+        d3.select('#tab1').style('background-color','buttonface');
+});
