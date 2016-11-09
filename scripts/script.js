153c85ba536e4de698fb5149da97b8a7be862f3a
ashwinijanamatti <ashwini.janamatti@utah.edu>
2016-11-08 22:06:47 -0700

    8th november 22:06

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ d3.select('#tab1')
 
     d3.select('#content2').attr('class','hide');
     d3.select('#content1').attr('class','show');
-    d3.select('#tab1').style('background-color','steelblue');
+    d3.select('#tab1').style('background-color','teal');
     d3.select('#tab2').style('background-color','buttonface');
 });
 
@@ -16,6 +16,15 @@ d3.select('#tab2')
 
         d3.select('#content1').attr('class','hide');
         d3.select('#content2').attr('class','show');
-        d3.select('#tab2').style('background-color','steelblue');
+        d3.select('#tab2').style('background-color','teal');
         d3.select('#tab1').style('background-color','buttonface');
+});
+
+d3.csv("data/movie_metadata.csv", function (error, csvData) {
+
+    csvData.forEach(function(d){
+
+        d.actors = d.actor_1_name + ',' + d.actor_2_name + ',' + d.actor_3_name;
+    });
+    console.log(csvData[0]);
 });
