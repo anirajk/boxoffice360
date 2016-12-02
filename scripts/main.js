21638fb7a7dcebb7fc053ffb2d897ab75a9696f1
ashwinijanamatti <ashwini.janamatti@utah.edu>
2016-12-01 23:02:06 -0700

    saving ashwini's changes

--- scripts/main.js
+++ scripts/main.js
@@ -14,8 +14,105 @@
 
 var sortkeys = ["Sort by Rating", "Sort by Movie Facebook Likes", "Sort by Budget", "Sort by Duration"];
 var selectedYear=1985;
+var moviesData;
+
+
+function updateBarChart(data) {
+
+    var margin = {top: 20, right: 20, left: 20, bottom: 20};
+
+    var svg = d3.select("#barChartsvg"),
+        xAxisWidth = 650,
+        yAxisHeight = 450;
+
+
+
+
+    var width = xAxisWidth - margin.left - margin.right;
+    var height = yAxisHeight - margin.top - margin.bottom;
+
+
+    svg.attr('width', width)
+        .attr('height',height);
+
+
+    var highGrossingData = data.sort(function(a,b){
+
+        return b.gross - a.gross;
+    });
+
+    var top5HighGrossingData = highGrossingData.slice(0,5);
+
+
+    var max = d3.max(top5HighGrossingData, function(d){
+
+
+        return d.gross;
+    })        ;
+
+
+    var xScale = d3.scaleLinear()
+        .domain([0 , max])
+        .range([0,width]);
+
+    console.log('xscale max = '+max);
+
+    var yScale = d3.scaleBand()
+        .range([0, height]).padding(.1);
+
+    var xAxis = d3.axisBottom();
+    xAxis.scale(xScale);
+
+
+    yScale.domain(data.map(function (d) {
+        return d.movie_title;
+    }));
+
+    var yHeight = (450 - margin.top - margin.bottom)/5;
+
+    var bars = d3.select('#barChartg')
+        .selectAll('.bars')
+        .data(top5HighGrossingData);
+
+    bars.exit()
+        .remove();
+
+    bars = bars.enter()
+        .append('rect')
+        .merge(bars);
+
+    var textWidth = 80;
+
+    bars.attr('class','bars')
+        .attr('x', textWidth)
+        .attr('y', function(d,i){
+
+            if(i==0)
+                return 0;
+            else
+                return (yHeight + 4)*i;
+        })
+        .attr('width',function (d) {
+
+            if(d.gross)
+                return xScale(d.gross) - xScale(0);
+            else
+                return 0;
+        })
+        .attr('height', function (d) {
+
+            return yHeight;
+
+        });
+
+}
+
+
 d3.csv("data/movie_metadata.csv", function (error, csvData) {
     var yearList = [];
+
+    moviesData = csvData;
+
     csvData.forEach(function (d, i) {
         d.actors = d.actor_1_name + ',' + d.actor_2_name + ',' + d.actor_3_name;
         if (yearList.indexOf(d.title_year) === -1) {
@@ -24,6 +121,8 @@ d3.csv("data/movie_metadata.csv", function (error, csvData) {
             }
         }
     });
+
+
     yearList.sort();
     var slider = d3.slider()
         .min(d3.min(yearList))
@@ -33,8 +132,9 @@ d3.csv("data/movie_metadata.csv", function (error, csvData) {
         .showRange(true)
         .value(d3.max(yearList))
         .callback(updateOnSliderChange);
+
     d3.select('#year-slider').call(slider);
-    console.log("len of csv data", csvData.length);
+
     function updateOnSliderChange(slider) {
         var year = Math.ceil(slider.value());
         selectedYear = year;
@@ -75,15 +175,15 @@ d3.csv("data/movie_metadata.csv", function (error, csvData) {
             }
             buttons.appendChild(but);
 
+            updateBarChart(yearData);
+
         }
 
     }
 
-})
+});
     function updateVis(yearData, text) {
-        console.log("Going to update visualisation with",text);
-        //console.log("it has ", yearData.length, " movies");
-        //console.log("first movie", yearData[0])
+
         var requests = yearData.length;
         generateBlanktilesandButtons(yearData.length);
         var i = 0;
@@ -109,7 +209,7 @@ d3.csv("data/movie_metadata.csv", function (error, csvData) {
     }
 
     function generateBlanktilesandButtons(n) {
-        console.log("generating ", n, " blank tiles");
+        //console.log("generating ", n, " blank tiles");
         var alltiles = document.getElementById("movie-tiles");
         while (alltiles.firstChild) {
             alltiles.removeChild(alltiles.firstChild);
@@ -133,7 +233,7 @@ d3.csv("data/movie_metadata.csv", function (error, csvData) {
             div.appendChild(img);
             div.appendChild(txt);
             alltiles.appendChild(div);
-        }
+       }
 
 
     }
@@ -172,6 +272,8 @@ d3.csv("data/movie_metadata.csv", function (error, csvData) {
     function updateImage(tileid, url, title) {
         console.log("updating " + tileid + " with ", title);
         var tile = d3.select("#" + tileid).select("img");
+
+        var tile = d3.select("#" + tileid);
         tile.classed("movie-tile", true);
         var image = document.getElementById(tileid).firstChild;
         var downloadingImage = new Image();
@@ -184,5 +286,42 @@ d3.csv("data/movie_metadata.csv", function (error, csvData) {
         var txt=d3.select('#'+tileid).select("#txt-"+tileid);
         txt.innerHTML = title;
 
+
+        image.addEventListener('click',function(d){
+
+            var movie = moviesData.filter(function(d){
+
+                return d.movie_title.toLowerCase() == title.toLowerCase();
+            });
+
+            d3.select('#movie')
+                .text(function() {
+
+                    return 'Movie: ' + movie[0].movie_title;
+
+                });
+
+            d3.select('#director')
+                .text(function(){
+
+                    if(movie[0].director_name)
+                        return 'Director: ' + movie[0].director_name;
+                    else
+                        'Director: ' + 'Not Available';
+                });
+
+            d3.select('#actors')
+                .text(function(){
+
+                    return 'Actors: ' + movie[0].actor_1_name +' , '+ movie[0].actor_2_name +' , '+ movie[0].actor_3_name;
+                });
+
+            d3.select('#genre')
+                .text(function(){
+
+                    return 'Genre: ' + movie[0].genres;
+                });
+
+        });
     }
 
